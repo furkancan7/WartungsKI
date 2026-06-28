e83939c97208ed775e3c36393e414853e8fb9d1b
Furkan Can <furkancancelik480@gmail.com>
2026-06-29 01:15:49 +0300

    Delete wartungski directory

--- wartungski/src/main/java/com/fc/wartungski/MachineRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fc.wartungski;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface MachineRepository extends JpaRepository<Machine, Long> {
-    List<Machine> findAllByOrderByTimestampDesc();
-}
